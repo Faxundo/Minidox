f5466ef0111032a0db8ebde1b966e9b57bdea40e
Facundo Cuadrado <hola9811@hotmail.com>
2022-10-05 22:20:41 -0300

    Initial commit

---
+++ src/main/java/net/faxu/minidox/Minidox.java
@@ -0,0 +1,14 @@
+package net.faxu.minidox;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Minidox implements ModInitializer {
+	public static final String MOD_ID = "minidox";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/faxu/minidox/MinidoxClient.java
@@ -0,0 +1,10 @@
+package net.faxu.minidox;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MinidoxClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
